6cee54471c488d14a0c0e05f6c9521afdcdf6c15
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2018-01-14 02:48:34 +0100

    Refactoring

--- app/src/main/java/com/example/username/hakatonindoorway/NullActivity.java
+++ app/src/main/java/com/example/username/hakatonindoorway/NullActivity.java
@@ -5,9 +5,10 @@ import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -21,8 +22,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
 public class NullActivity extends AppCompatActivity implements TextToSpeech.OnInitListener,
         LoaderManager.LoaderCallbacks<List<DayCoursesDto>>{
 
@@ -35,7 +34,9 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private int eventPosition;
     private List<DayCoursesDto> courses;
 
-    boolean optionSelected;
+    private boolean optionSelected;
+    private boolean force;
+    private State state;
 
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd MMMM");
     private static final DateFormat TIME_FORMAT = new SimpleDateFormat("H mm");
@@ -48,6 +49,33 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
         speaker = new TextToSpeech(this, this);
         root = findViewById(R.id.root);
         optionSelected = false;
+        state = State.INIT;
+
+        final GestureDetectorCompat gd = new GestureDetectorCompat(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(velocityY > 1 && Math.abs(velocityY) > Math.abs(velocityX))
+                    setState(State.MENU, true);
+                if(velocityY < 1 && Math.abs(velocityY) > Math.abs(velocityX)) {
+                    speaker.speak(getString(R.string.user_guide), TextToSpeech.QUEUE_ADD, null,
+                            "_");
+                    setState(State.MENU, false);
+                }
+                if(velocityX > 1 && Math.abs(velocityX) > Math.abs(velocityY))
+                    moveEvents(1, true);
+                if(velocityX < 1 && Math.abs(velocityX) > Math.abs(velocityY))
+                    moveEvents(-1, true);
+                return true;
+            }
+        });
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(state == State.READ_LIST || state == State.MENU)
+                    return gd.onTouchEvent(motionEvent);
+                return false;
+            }
+        });
     }
 
     @Override
@@ -66,13 +94,12 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
                             ++nClick;
                             if (nClick == 2) {
                                 optionSelected = true;
-                                if ("nextDay".equals(s)) {
+                                if (state == State.NEXT_DAY) {
                                     ++dayPosition;
                                     eventPosition = 0;
-                                    speaker.speak(DATE_FORMAT.format(courses.get(dayPosition).getDate()),
-                                            TextToSpeech.QUEUE_ADD, null, "_");
+                                    setState(State.READ_LIST, false);
                                 } else {
-                                    speaker.speak(getString(R.string.guide), TextToSpeech.QUEUE_ADD, null, "super");
+                                    setState(State.GUIDE, false);
                                 }
                             }
                         }
@@ -89,19 +116,12 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
                             root.setOnClickListener(null);
                             if(optionSelected && !"nextDay".equals(s))
                                 return;
-                            if("hello".equals(s) || ("nextDay".equals(s) && !optionSelected))
-                                speaker.speak(getString(R.string.menu), TextToSpeech.QUEUE_ADD, null, "menu");
+                            if("hello".equals(s) || ("nextDay".equals(s) && !optionSelected)) {
+                                setState(State.MENU, false);
+                            }
                             if(!"readList".equals(s) && !(optionSelected && "nextDay".equals(s)))
                                 return;
-                            ++eventPosition;
-                            if(eventPosition == courses.get(dayPosition).getEvents().size()) {
-                                if(dayPosition == courses.size())
-                                    speaker.speak(getString(R.string.menu), TextToSpeech.QUEUE_ADD, null, "menu");
-                                speaker.speak(getString(R.string.next_day), TextToSpeech.QUEUE_ADD, null, "nextDay");
-                                return;
-                            }
-                            String msg = formatMessage(courses.get(dayPosition).getEvents().get(eventPosition));
-                            speaker.speak(msg, TextToSpeech.QUEUE_ADD, null, "readList");
+                            moveEvents(1, false);
                         }
                     }, 2000);
                 }
@@ -112,10 +132,7 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
                     root.setOnLongClickListener(new View.OnLongClickListener() {
                         @Override
                         public boolean onLongClick(View view) {
-                            speaker.speak(DATE_FORMAT.format(courses.get(dayPosition).getDate()),
-                                    TextToSpeech.QUEUE_ADD, null, "_");
-                            String msg = formatMessage(courses.get(dayPosition).getEvents().get(eventPosition));
-                            speaker.speak(msg, TextToSpeech.QUEUE_ADD, null, "readList");
+                            setState(State.READ_LIST, false);
                             root.setOnLongClickListener(null);
                             return true;
                         }
@@ -123,7 +140,7 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 }
 
                 if ("super".equals(s)) {
-                    speaker.speak(getString(R.string.menu), TextToSpeech.QUEUE_ADD, null, "menu");
+                    setState(State.MENU, false);
                 }
             }
 
@@ -134,6 +151,57 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
         });
         speaker.setLanguage(Locale.forLanguageTag("pl"));
         getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    private void setState(State state, boolean force) {
+        int queueMode = TextToSpeech.QUEUE_ADD;
+        if(force)
+            queueMode = TextToSpeech.QUEUE_FLUSH;
+
+        if(state == State.HELLO) {
+            dayPosition = startDayPosition;
+            eventPosition = startEventPosition;
+            CourseDto course = courses.get(dayPosition).getEvents().get(eventPosition);
+            long diff = (course.getStartTime().getTime()  - System.currentTimeMillis()) / (60*1000);
+            String msg = getString(R.string.course_info1, course.getName(), diff, course.getRoom());
+            speaker.speak(msg, queueMode, null, "hello");
+        } else if(state == State.MENU) {
+            speaker.speak(getString(R.string.menu), queueMode, null, "menu");
+        } else if(state == State.READ_LIST) {
+            if(this.state != State.READ_LIST) {
+                speaker.speak(DATE_FORMAT.format(courses.get(dayPosition).getDate()),
+                        queueMode, null, "_");
+            }
+            String msg = formatMessage(courses.get(dayPosition).getEvents().get(eventPosition));
+            speaker.speak(msg, queueMode, null, "readList");
+        } else if(state == State.NEXT_DAY) {
+            speaker.speak(getString(R.string.next_day), queueMode, null, "nextDay");
+        } else if(state == State.GUIDE) {
+            speaker.speak(getString(R.string.guide), queueMode, null, "super");
+        }
+
+        this.state = state;
+    }
+
+    private void moveEvents(int diff, boolean force) {
+        eventPosition += diff;
+        if(eventPosition == courses.get(dayPosition).getEvents().size()) {
+            if(dayPosition == courses.size()) {
+                setState(State.MENU, force);
+            } else {
+                setState(State.NEXT_DAY, force);
+            }
+            return;
+        }
+        if(eventPosition < 0) {
+            --dayPosition;
+            if(dayPosition <= startDayPosition) {
+                dayPosition = startDayPosition;
+                eventPosition = startEventPosition;
+            } else
+                eventPosition = 0;
+        }
+        setState(State.READ_LIST, force);
     }
 
     @Override
@@ -151,10 +219,7 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
         startDayPosition = dayPosition;
         startEventPosition = eventPosition;
         if(dayPosition < data.size()) {
-            CourseDto course = courses.get(dayPosition).getEvents().get(eventPosition);
-            long diff = (course.getStartTime().getTime()  - System.currentTimeMillis()) / (60*1000);
-            String msg = getString(R.string.course_info1, course.getName(), diff, course.getRoom());
-            speaker.speak(msg, TextToSpeech.QUEUE_ADD, null, "hello");
+            setState(State.HELLO, false);
         }
     }
 
@@ -163,7 +228,7 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
     }
 
-    public void moveToNextEvent() {
+    private void moveToNextEvent() {
         ++eventPosition;
         if(eventPosition == courses.get(dayPosition).getEvents().size()) {
             ++dayPosition;
@@ -171,9 +236,13 @@ public class NullActivity extends AppCompatActivity implements TextToSpeech.OnIn
         }
     }
 
-    public String formatMessage(CourseDto course) {
+    private String formatMessage(CourseDto course) {
         String msg = getString(R.string.course_info2, TIME_FORMAT.format(course.getStartTime()),
                 course.getName(), course.getRoom());
         return msg;
+    }
+
+    private enum State {
+        INIT, HELLO, MENU, READ_LIST, NEXT_DAY, GUIDE
     }
 }
